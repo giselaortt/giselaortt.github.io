477fa8b0bdbf1510d44e50b4296c7544a81812f4
Gisela Moreira Ortt <giortt@amazon.com>
2022-02-07 14:00:39 -0300

    test

--- js/main.js
+++ js/main.js
@@ -6,34 +6,6 @@ var main = {
   numImgs : null,
 
   init : function() {
-    // Shorten the navbar after scrolling a little bit down
-    $(window).scroll(function() {
-        if ($(".navbar").offset().top > 50) {
-            $(".navbar").addClass("top-nav-short");
-        } else {
-            $(".navbar").removeClass("top-nav-short");
-        }
-    });
-    
-    // On mobile, hide the avatar when expanding the navbar menu
-    $('#main-navbar').on('show.bs.collapse', function () {
-      $(".navbar").addClass("top-nav-expanded");
-    });
-    $('#main-navbar').on('hidden.bs.collapse', function () {
-      $(".navbar").removeClass("top-nav-expanded");
-    });
-	
-    // On mobile, when clicking on a multi-level navbar menu, show the child links
-    $('#main-navbar').on("click", ".navlinks-parent", function(e) {
-      var target = e.target;
-      $.each($(".navlinks-parent"), function(key, value) {
-        if (value == target) {
-          $(value).parent().toggleClass("show-children");
-        } else {
-          $(value).parent().removeClass("show-children");
-        }
-      });
-    });
     
     // Ensure nested navbar menus are not longer than the menu header
     var menus = $(".navlinks-container");
--- pages/books.md
+++ pages/books.md
@@ -3,8 +3,10 @@ layout: page
 title: Books
 use-site-title: true
 subtitle: this is my personal bookshelf
+bigimg: ../images/capas/gavea.jpg
+cover-img: ../images/capas/gavea.jpg
 ---
-[//]: # bigimg: ../images/capas/gavea.jpg
+[//]: # 
 [//]: #  - [<u>Recomendations</u>](recomendations): My favorite books
 [//]: #  - [<u>Reading</u>](reading): My reading list
 
